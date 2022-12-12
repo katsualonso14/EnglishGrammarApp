4eeb5714fd20c967e22c07e034326c9c3f2fab0c
KatsuyaTamai <katsuyatamai@MacBook-Pro.local>
2022-12-12 19:06:28 +0900

    First Commit

---
+++ GrammarApp/ViewController.swift
@@ -0,0 +1,16 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        button.backgroundColor = .red
+        button.setTitle("Test", for: .normal)
+        view.addSubview(button)
+        
+    }
+}
+
